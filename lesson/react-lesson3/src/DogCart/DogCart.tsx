712015ab9fcaa2859b8e1917da174f790a2054f4
Kenan Iusubovi <te666za@gmail.com>
2023-10-25 20:28:57 +0200

    add

--- lesson/react-lesson3/src/DogCart/DogCart.tsx
+++ lesson/react-lesson3/src/DogCart/DogCart.tsx
@@ -1,23 +1,19 @@
 import React, {FC, useEffect, useState} from "react";
 import MyButton from "../MyButton/MyButton";
 
-
 const DogCart: FC = () => {
     const [img, setImg] = useState(' ');
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('https://dog.ceo/api/breeds/image/random')
             .then((res) => res.json())
-            .then((data) =>  console.log(data));
+            .then((data) => console.log(data));
     }, []);
 
     return (
         <div>
             <h1>DogCart</h1>
-            <img src={data?.message} alt=" dog picture ">
-                <MyButton onClick={fetchDog} text="Referesh Dog" />
+            <img src={} alt=""/>
         </div>
-)
+    )
 }
-
-export default DogCart;
