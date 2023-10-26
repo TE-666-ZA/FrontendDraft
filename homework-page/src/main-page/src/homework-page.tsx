ebe722590aa7d1a3e3cf5795315a8b27e70cdcf8
Kenan Iusubovi <te666za@gmail.com>
2023-10-26 12:20:12 +0200

    feat: add day night mode toggle

--- homework-page/src/main-page/src/homework-page.tsx
+++ homework-page/src/main-page/src/homework-page.tsx
@@ -1,12 +1,21 @@
 import './homework-page.css';
+import React from 'react';
+import Toggle from "../toggle/toggle";
+
 
 function HomeworkPage() {
+    const [toggled, setToggled] = React.useState(false);
+    const hundleClick = () => {
+        setToggled((s) => !s);
+    }
     return (
         <div className="container">
-            <header className="header">
-
+            <header className="title">
                 <h1>Homepage</h1>
             </header>
+            <nav className="navbar">
+                <Toggle toggled={toggled} onClick={hundleClick}/>
+            </nav>
 
         </div>
     );
