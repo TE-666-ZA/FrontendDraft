0e34f5b884d128aea964b46b821c193f788696d6
Kenan Iusubovi <te666za@gmail.com>
2023-10-26 10:23:42 +0200

    feat: initial commit homework-page project

---
+++ homework-page/src/main-page/src/homework-page.tsx
@@ -0,0 +1,15 @@
+import './homework-page.css';
+
+function HomeworkPage() {
+    return (
+        <div className="container">
+            <header className="header">
+
+                <h1>Homepage</h1>
+            </header>
+
+        </div>
+    );
+}
+
+export default HomeworkPage;
